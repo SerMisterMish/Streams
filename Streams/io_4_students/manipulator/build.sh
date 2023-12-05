1370bba53b4c57d0b7be4c5d2443f61b126f7f09
Khromov Nikita <38394723+SerMisterMish@users.noreply.github.com>
2023-12-05 21:24:18 +0300

    Delete io_4_students directory

--- Streams/io_4_students/manipulator/build.sh
+++
@@ -1,1 +0,0 @@
-g++ -Wall -O2 --std=c++11 -o manipulator manipulator.cpp
--- Streams/io_4_students/stringstream/build.sh
+++
@@ -1,3 +0,0 @@
-g++ -Wall -O2 --std=c++11 -o istring istring.cpp
-g++ -Wall -O2 --std=c++11 -o ostring ostring.cpp
-g++ -Wall -O2 --std=c++11 -o stringseeking stringseeking.cpp
